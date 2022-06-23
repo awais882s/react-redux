fd296e8eeaeace3f7c92b45b34ea40deacdac6fc
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-23 20:28:02 +0500

    make student reducers

---
+++ src/Redux/reducers/studentReducers.jsx
@@ -0,0 +1,27 @@
+
+
+let initialState = {
+    name: "naveed",
+    age: 20,
+    class: "1st",
+    students: [],
+}
+function StudentReducers(state = initialState, action) {
+    switch (action.type) {
+        case "ADD_STUDENT":
+            // work will / logic will here
+            let newState = {
+                ...state,
+                name: "ali",
+                newData: action.payload
+            }
+            return newState
+
+
+        default:
+            return state;
+    }
+
+
+}
+export default StudentReducers;
--- src/Redux/rootReducer.jsx
+++ src/Redux/rootReducer.jsx
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
-import studentsReducer from "./studentsReducer";
+import authReducer from "./authReducer";
+import studentReducers from "./studentReducers";
 const rootReducer = combineReducers({
+    authReducer,
+    studentReducers 
 
 })
 export default rootReducer;
