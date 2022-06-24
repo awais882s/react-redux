4cfe77a4cf23b17118863d0561adf9301428af9d
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-24 22:55:51 +0500

    Update store.jsx

--- src/config/store.jsx
+++ src/config/store.jsx
@@ -2,5 +2,4 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducers from "../Redux/rootReducers";
 const store = createStore(rootReducers, applyMiddleware(thunk));
-
 export default store;
